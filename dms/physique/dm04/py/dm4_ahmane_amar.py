a86283b9fca917ed51f067f48acbb5cdb280f9e1
Tiplouf <amarourama@yahoo.fr>
2021-11-11 18:04:10 +0100

    dm4 de physique

---
+++ dms/physique/dm04/py/dm4_ahmane_amar.py
@@ -0,0 +1,33 @@
+"""Programme de résolution numérique d'une équation différentielle d'ordre 2
+"""
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Defining private constants
+_OMEGA_0 = 1.61e+3
+_Q = 14
+_OMEGA = _OMEGA_0*np.sqrt(1-1/(4*(_Q**2)))
+_MU = _OMEGA_0/(2*_Q)
+_E = 12.
+_R = 6.
+_C = 1e-5
+_Q0 = _E*_C
+_N = 1000
+
+
+def charge_primaire(V, t):
+	"""Fonction demandée à la question 8.
+	"""
+	x, y = V
+	return [y, (_OMEGA_0**2)*(_Q0 - x)]
+
+
+if __name__ == "__main__":	
+	t = np.linspace(0, 0.1, _N)
+	result = odeint(charge_primaire, [0, 0], t)
+	plt.plot(t, result[:,0], "-r", label="num")
+	plt.plot(t, (-_Q0*np.cos(_OMEGA*t)-(_MU*_Q0/_OMEGA)*np.sin(_OMEGA*t))*np.exp(-_MU*t) + _Q0, "-b", label="anal")
+	plt.legend()
+	plt.show()
+
